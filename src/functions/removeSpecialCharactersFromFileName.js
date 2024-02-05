820dc849656fc4393065d838bfaeb50038430ca8
Rakshith V S <rakshith@betalectic.com>
2024-02-05 17:18:51 +0530

    added a function to remove special characters from file name

---
+++ src/functions/removeSpecialCharactersFromFileName.js
@@ -0,0 +1,5 @@
+module.exports = function (inputString) {
+  const nonAlphanumericRegex = /[^a-zA-Z0-9]/g;
+  const alphanumericString = inputString.replace(nonAlphanumericRegex, "");
+  return alphanumericString;
+};
