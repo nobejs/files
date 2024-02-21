73855a04fba6364b2463906d73e5750bb2ffab83
Rakshith V S <rakshith@betalectic.com>
2024-02-21 14:31:18 +0530

    updated regex to allow . in file name

--- src/functions/removeSpecialCharactersFromFileName.js
+++ src/functions/removeSpecialCharactersFromFileName.js
@@ -1,5 +1,5 @@
 module.exports = function (inputString) {
-  const nonAlphanumericRegex = /[^a-zA-Z0-9]/g;
+  const nonAlphanumericRegex = /[^a-zA-Z0-9](?=.*\.)/g;
   const alphanumericString = inputString.replace(nonAlphanumericRegex, "");
   return alphanumericString;
 };
